d575a30be7c13a3531377b1473784478a91c7add
PerZZi <emiliano.estebandiaz@gmail.com>
2024-10-15 17:49:58 -0300

    Âprimer commit

---
+++ src/main/java/com/example/GuiaSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaSpringApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/example/models/Users.java
@@ -0,0 +1,35 @@
+package com.example.models;
+
+import com.example.models.enums.Rol;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private Rol rol = Rol.CLIENT;
+
+    public Users(String name, String lastName, String email, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
